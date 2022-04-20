1912fd4568edbe9a9db1c34f7a6847ba3437b813
iury simoes-sousa <simoesiury@gmail.com>
2022-04-20 02:16:05 -0400

    change from Field to field

--- src/cumulative_vertical_integration.jl
+++ src/cumulative_vertical_integration.jl
@@ -5,25 +5,26 @@ using Oceananigans.Utils: launch!
 using Oceananigans.Grids
 using Oceananigans.Operators: Δzᶜᶜᶜ
 
-@kernel function _cumulative_vertical_integration!(Field, grid)
+@kernel function _cumulative_vertical_integration!(field, grid)
     i, j = @index(Global, NTuple)
 
     integ = 0
     @unroll for k in 1 : grid.Nz
-        integ = integ + @inbounds Field[i, j, k] * Δzᶜᶜᶜ(i, j, k, grid)
-        @inbounds Field[i, j, k] = integ
+        integ = integ + @inbounds field[i, j, k] * Δzᶜᶜᶜ(i, j, k, grid)
+        @inbounds field[i, j, k] = integ
     end
 end
 
-function cumulative_vertical_integration!(Field)
-    grid = Field.grid
+function cumulative_vertical_integration!(input_field)
+    field = Field(input_field)
+    grid = field.grid
     arch = architecture(grid)
 
     event = launch!(arch, grid, :xy,
-                    _cumulative_vertical_integration!, Field, grid,
+                    _cumulative_vertical_integration!, field, grid,
                     dependencies = device_event(arch))
 
     wait(device_event(arch), event)
 
-    return nothing
+    return field
 end
