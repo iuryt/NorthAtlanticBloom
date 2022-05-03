f4bf322074260556d86b333106de3a7393e4fd58
iury simoes-sousa <simoesiury@gmail.com>
2022-05-02 22:36:02 -0400

    change to 12 days

--- src/01-model/03-initial_conditions_coarse.py
+++ src/01-model/03-initial_conditions_coarse.py
@@ -4,10 +4,10 @@ import numpy as np
 ds = xr.open_dataset("../../data/raw/output_submesoscale.nc")
 ds = ds.assign_coords(time=ds.time.astype("float")*1e-9/86400)
     
-days = 5
+days = 12
 new_resolution_km = 10 
 
-dsi = ds.sel(time=days).interp(xF=ds.xC, yF=ds.yC)
+dsi = ds.sel(time=days, method="nearest").interp(xF=ds.xC, yF=ds.yC)
 
 
 dsi_periodic = xr.concat([
