56bf544251ab0b88d8b3c7c7b1df944163051a54
iury simoes-sousa <simoesiury@gmail.com>
2022-05-04 21:25:03 -0400

    define and plot data-driven initial conditions

--- src/01-model/01-initial_conditions.py
+++ src/01-model/01-initial_conditions.py
@@ -1,81 +1,118 @@
-from argopy import DataFetcher as ArgoDataFetcher
 import numpy as np
 import matplotlib.pyplot as plt
-import seawater as sw
 import xarray as xr
+from xhistogram.xarray import histogram
 from scipy.optimize import curve_fit
-from os.path import exists
 
-BOX = [-25, -15, 58, 62]
 
-if not(exists("../../data/interim/argo_north_atlantic.nc")):
-    argo = ArgoDataFetcher().region([*BOX, 0, 1000, '2010-01', '2020-12']).to_xarray()
-    argo = argo.argo.point2profile().argo.interp_std_levels(np.arange(0,1000))
-
-    argom = argo.groupby("TIME.month").mean("N_PROF").sel(month=2) #FEB
-    argom = argom.assign(PDEN=("PRES_INTERPOLATED",sw.pden(argom.PSAL,argom.TEMP,argom.PRES_INTERPOLATED)))
-    argom = argom.assign(B=-(9.82/1025)*argom.PDEN)
-    argom = argom.rolling(PRES_INTERPOLATED=50,min_periods=1).mean()
-
-    argom.to_netcdf("../../data/interim/argo_north_atlantic.nc")
-else:
-    argom = xr.open_dataset("../../data/interim/argo_north_atlantic.nc")
+chla = xr.open_dataset("../../data/interim/bioargo_north_atlantic.nc")
+no3 = xr.open_dataset("../../data/interim/woa18_north_atlantic.nc")
+shortwave = xr.open_dataarray("../../data/interim/shortwave_north_atlantic.nc")
+argo = xr.open_dataset("../../data/interim/argo_north_atlantic.nc").mean("N_PROF")
     
     
 def func(z, a, b, c,d,e):
     return a * np.tanh(b*(z-c)) + d*z**2 + e
 
-popt_argo, pcov_argo = curve_fit(func, argom.PRES_INTERPOLATED.values, argom.PDEN.values,p0=[0.2,6e-3,500,0,1027.6])
-argom = argom.assign(PDENf=("PRES_INTERPOLATED",func(argom.PRES_INTERPOLATED.values,*popt_argo)))
-argom = argom.assign(Bf=-(9.82/1025)*argom.PDENf)
+popt_argo, pcov_argo = curve_fit(func, argo.PRES_INTERPOLATED.values, argo.PDEN.values,p0=[0.2,6e-3,500,0,1027.6])
+argo = argo.assign(PDENf=("PRES_INTERPOLATED",func(argo.PRES_INTERPOLATED.values,*popt_argo)))
+argo = argo.assign(Bf=-(9.82/1025)*argo.PDENf)
 title_argo =  f"{popt_argo[0]:.4f} tanh( {popt_argo[1]:.06f}( z-{popt_argo[2]:.03f} ) ) {popt_argo[3]*1e5:.3f} 10$^5$ z + {popt_argo[4]:.3f}"
 
 print(title_argo)
 
 
 
-# https://psl.noaa.gov/data/gridded/data.ncep.reanalysis.derived.surfaceflux.html
-shortwave = xr.open_dataset("../../data/external/dswrf_sfc_mon_mean.nc")
-shortwave = shortwave.assign(lon=(((shortwave.lon + 180) % 360) - 180)).sortby("lon")
-
-shortwave_month = shortwave.sel(lon=np.mean(BOX[:2]), lat=np.mean(BOX[2:]), method="nearest").dswrf.groupby("time.month").mean()
-shortwave_yearday = shortwave_month.assign_coords(month=np.linspace(0,365,12)).rename(month="yearday")
-
 def func_shortwave(t, a, b, c, d):
     return a * np.sin(2*np.pi*t/b - c) + d
 
-popt_shortwave, pcov_shortwave = curve_fit(func_shortwave, shortwave_yearday.yearday.values, shortwave_yearday.values, p0=[200, 365, 0, 100])
+popt_shortwave, pcov_shortwave = curve_fit(func_shortwave, shortwave.yearday.values, shortwave.values, p0=[200, 365, 0, 100])
 a, b, c, d = np.round(popt_shortwave,decimals=1)
 
 
-fig, ax = plt.subplots(1, 2, figsize=(6.5,3))
 
-_ = (argom.PDEN-1000).plot.line(ax=ax[0], y="PRES_INTERPOLATED", ylim=[1000,0], lw=4, color="0.1")
-_ = (argom.PDENf-1000).plot.line(ax=ax[0], y="PRES_INTERPOLATED", ylim=[1000,0], linestyle="--", color="red")
-ax[0].set(
-    xlabel="$\sigma_\\theta$ [kg/m$^{3}$]",
-    ylabel="z [m]",
-    yticks=np.arange(0, 1000, 200),
-    yticklabels=-np.arange(0, 1000, 200),
-    title=title_argo
+h_no3 = histogram(no3.n_an, bins=np.arange(7,22,0.5), dim=["profile"]).rolling(depth=5, center=True, min_periods=1).mean()
+h_no3 = h_no3.assign_coords(depth=-h_no3.depth).rename(depth="z")
+cdf_no3 = h_no3.cumsum("n_an_bin")/h_no3.sum("n_an_bin")
+
+dc = 0.022
+h_chla = histogram(-chla.PRES, chla.CPHL_ADJUSTED, bins=[np.arange(-1000,0+10,10), np.arange(-0.02-dc/2,0.2,dc)])
+cdf_chla = h_chla.cumsum("CPHL_ADJUSTED_bin")/h_chla.sum("CPHL_ADJUSTED_bin")
+cdf_chla = cdf_chla.assign_coords(CPHL_ADJUSTED_bin=cdf_chla.CPHL_ADJUSTED_bin+dc/2)
+
+
+
+colors = {
+    "obs": "#1E88E5",
+    "model": "#D81B60",
+}
+
+fig = plt.figure(figsize=(6,6), constrained_layout=True)
+ax = fig.subplot_mosaic(
+    [
+        ["pden", "chla"],
+        ["no3", "shortwave"],
+    ],
 )
-ax[0].plot([], [], color="0.1", label="Argo")
-ax[0].legend(loc=1, fontsize=8)
+
+_ = (argo.PDEN-1000)[::100].plot.line(ax=ax["pden"], y="PRES_INTERPOLATED", ylim=[1000,0], lw=0, marker="o", color=colors["obs"])
+_ = (argo.PDENf-1000).plot.line(ax=ax["pden"], y="PRES_INTERPOLATED", ylim=[1000,0], color=colors["model"])
+ax["pden"].text(0.5, 0.02, "Argo", ha="center", color="0.3", fontsize=12, transform=ax["pden"].transAxes)
+
+cdf_chla.plot.contourf(ax=ax["chla"], levels=[0.2,0.8], colors=colors["obs"], alpha=0.5, extend="neither", add_colorbar=False)
+cdf_chla.plot.contour(ax=ax["chla"], levels=[0.5], colors=colors["obs"])
+ax["chla"].text(0.5, 0.02, "BioArgo", ha="center", color="0.3", fontsize=12, transform=ax["chla"].transAxes)
+
+cdf_no3.plot.contourf(ax=ax["no3"], levels=[0.2,0.8], colors=colors["obs"], alpha=0.5, extend="neither", add_colorbar=False)
+cdf_no3.plot.contour(ax=ax["no3"], levels=[0.5], colors=colors["obs"])
+ax["no3"].text(0.3, 0.02, "WOA18", ha="center", color="0.3", fontsize=12, transform=ax["no3"].transAxes)
+
+zi = -np.arange(1000)
+ax["chla"].plot(0.1*(np.tanh(1e-2*(zi+300))+1)/2, zi, color=colors["model"])
+ax["no3"].plot(zi*(12-16)/(0+800)+12, zi, color=colors["model"])
 
 t0 = 50
-shortwave_yearday.plot(ax=ax[1], lw=4, color="0.1")
-ax[1].plot(np.arange(365), func_shortwave(np.arange(365), a, b, c, d), linestyle="--", color="red")
+shortwave.plot(ax=ax["shortwave"], lw=0, marker="o", color=colors["obs"])
+ax["shortwave"].plot(np.arange(365), func_shortwave(np.arange(365), a, b, c, d), color=colors["model"])
+ax["shortwave"].axvline(t0, color="0.2", linestyle="--")
+ax["shortwave"].text(0.5, 0.02, "NCEP", ha="center", color="0.3", fontsize=12, transform=ax["shortwave"].transAxes)
 
-ax[1].axvspan(t0, t0+90, color="0.7")
-ax[1].set(
-    ylabel="Shortwave Radiation [W/m$^{2}$]",
+ax["pden"].set(
+    xlabel="$\sigma_\\theta$ [kg m$^{-3}$]",
+    ylabel="z [m]",
+    yticks=np.arange(0,1000,200),
+    yticklabels=-np.arange(0,1000,200),
+    ylim=[1000,10],
+)
+
+ax["chla"].set(
+    ylabel="z [m]",
+    yticks=-np.arange(0,1000,200),
+    ylim=[-1000,-10],
+    xlabel="Chl-a [mg m$^{-3}$]",
+)
+
+ax["no3"].set(
+    ylabel="z [m]",
+    yticks=-np.arange(0,1000,200),
+    ylim=[-800,-10],
+    xlabel="Nitrate [mmol N m$^{-3}$]"
+)
+
+
+ax["shortwave"].set(
+    ylabel="Shortwave [W m$^{-2}$]",
     xlabel="Day of the year",
+    title="",
+    ylim=[20,280],
     xlim=[0,365],
     xticks=np.arange(0,365,50),
-    title=f"{a} sin( 2$\pi$ ( t + t$_0$ ) / {b} + {c} ) + {d}"
 )
-ax[1].plot([], [], color="0.1", label="NCEP")
-ax[1].legend(loc=1, fontsize=8)
-fig.tight_layout()
+# title=f"{a} sin( 2$\pi$ ( t + t$_0$ ) / {b} + {c} ) + {d}"
 
-fig.savefig("../../reports/figures/data_driven_initial_conditions.png")
+_ = [ax[k].grid(True, linestyle="--", alpha=0.5) for k in ax]
+letters = "a b c d".split()
+_ = [ax[k].set(title=f"{letter})"+50*" ") for k,letter in zip(ax, letters)]
+
+
+fig.savefig("../../reports/figures/data_driven_initial_conditions.png", facecolor="w", dpi=300, bbox_inches="tight")
